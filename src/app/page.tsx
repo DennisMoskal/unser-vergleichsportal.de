498a2d549d75f5fe034e52e62c9aefae153dd0a4
DennisMoskal <dennismoskal@yahoo.com>
2025-09-18 15:33:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -650,7 +650,7 @@ export default function Home() {
                   size="lg"
                   className="bg-green-600 text-white font-medium transition-all duration-300 ease-in-out hover:bg-green-700 hover:scale-105 hover:shadow-lg hover:bg-gradient-to-b hover:from-green-600 hover:to-green-700"
                 >
-                  Günstigen Tarif berechnen
+                  Finanzvergleich starten & besten Tarif finden
                 </Button>
               </a>
             </div>
