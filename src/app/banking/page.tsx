afefc871ac88b2ba0c9916badabf280ab424bff3
DennisMoskal <dennismoskal@yahoo.com>
2025-10-09 20:50:47 +0200

    Update page.tsx

--- src/app/banking/page.tsx
+++ src/app/banking/page.tsx
@@ -1338,7 +1338,7 @@ export default function Banking() {
           <ul className="space-y-2 text-sm sm:text-base text-gray-400">
             <li>
               <a
-                href="https://www.c24n.de/FZ9nd0R"
+                href="https://www.tarifcheck.com/NMXe4cX"
                 target="_blank"
                 rel="noopener noreferrer nofollow"
                 className="hover:text-white transition-all duration-300 ease-in-out hover:scale-105 rounded-lg hover:bg-gradient-to-b hover:from-gray-800 hover:to-gray-900 hover:shadow-sm"
